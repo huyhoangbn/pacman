02449edd7e9a5019ec921871f6c91cbd9ab028bc
huyhoangbn <42961840+huyhoangbn@users.noreply.github.com>
2019-10-22 23:29:32 +0700

    pacman2

--- Pacman2-Mutilagent/multiAgents.py
+++ Pacman2-Mutilagent/multiAgents.py
@@ -55,8 +55,8 @@ class ReflexAgent(Agent):
 
         "*** YOUR CODE HERE ***"
         Food=newFood.asList()
-        gPos=successorGameState.getGhostPositions()                         ###We have the position of the food and the ghost positions
-        FoodDist=[]                                                         ###We have the distance between the pacman,the food and the ghost
+        gPos=successorGameState.getGhostPositions()                        
+        FoodDist=[]                                                        
         GhostDist=[]
 
         for food in Food:
@@ -67,12 +67,12 @@ class ReflexAgent(Agent):
         if currentGameState.getPacmanPosition()==newPos:
             return(-(float("inf")))
 
-        for dist in GhostDist:                                              ###If the ghost is too near(next to pacman) we return(-float("inf")) like we have lost
+        for dist in GhostDist:                                            
             if dist<2:
-                return (-(float("inf")))                                    ###When there is no food left we return float("inf") like we have won
-        if len(FoodDist)==0:                                                ##Finally we return 1000/sum(FoodDist) + 10000/len(FoodDist) as the evaluation of the state
-            return float("inf")                                             ###It is not necessary that we put 1000 or 10000 ,the only need is to be large enough contrary to sum(foodDist)
-                                                                            ###and len(foodDist) respectively
+                return (-(float("inf")))                                   
+        if len(FoodDist)==0:                                            
+            return float("inf")                                            
+                                                                          
         return 1000/sum(FoodDist) +10000/len(FoodDist)
 
 
@@ -124,14 +124,14 @@ class MinimaxAgent(MultiAgentSearchAgent):
         "*** YOUR CODE HERE ***"
         def max_value(gameState,depth):
             Actions=gameState.getLegalActions(0)
-            if len(Actions)==0 or gameState.isWin() or gameState.isLose() or depth==self.depth:             ###The trvial situations(state)
+            if len(Actions)==0 or gameState.isWin() or gameState.isLose() or depth==self.depth:             
                 return(self.evaluationFunction(gameState),None)
-            w=-(float("inf"))                                                                               ###We are trying to implement the 2 sides of the minimax algorithm the max and the min
+            w=-(float("inf"))                                                                               
             Act=None
-            for action in Actions:                                                                          ###In that way that the 2 functions are calling each other is like building the tree(diagrams from tha class)
-                sucsValue=min_value(gameState.generateSuccessor(0,action),1,depth)                          #We have the available moves and we are seeking for the "best" one
-                sucsValue=sucsValue[0]                                                                      #It is working exactly as the theory of minimax algorithm commands
-                if(sucsValue>w):                                                                            #Here we have as start -infinite
+            for action in Actions:                                                                          
+                sucsValue=min_value(gameState.generateSuccessor(0,action),1,depth)                         
+                sucsValue=sucsValue[0]                                                                    
+                if(sucsValue>w):                                                                          
                     w,Act=sucsValue,action
             return(w,Act)
 
@@ -139,19 +139,19 @@ class MinimaxAgent(MultiAgentSearchAgent):
             Actions=gameState.getLegalActions(agentID)
             if len(Actions) == 0:
                 return(self.evaluationFunction(gameState),None)
-            l=float("inf")                                                                                  ###As we see in contrast with max we begin from +infinte
+            l=float("inf")                                                                                 
             Act=None
             for action in Actions:
                 if(agentID==gameState.getNumAgents() -1):
                     sucsValue=max_value(gameState.generateSuccessor(agentID,action),depth + 1)
                 else:
-                    sucsValue=min_value(gameState.generateSuccessor(agentID,action),agentID+1,depth)        ###We are doing exactly the opposite from the max "function"
+                    sucsValue=min_value(gameState.generateSuccessor(agentID,action),agentID+1,depth)        
                 sucsValue=sucsValue[0]
                 if(sucsValue<l):
                     l,Act=sucsValue,action
             return(l,Act)
         max_value=max_value(gameState,0)[1]
-        return max_value                                                                                    ###We are starting from the max and it goes as a tree max min max min
+        return max_value                                                                                   
 
 class AlphaBetaAgent(MultiAgentSearchAgent):
     """
@@ -170,8 +170,8 @@ class AlphaBetaAgent(MultiAgentSearchAgent):
 
             w=-(float("inf"))
             Act=None
-                                                                                                            ###We can see that the alpha beta agent is almost the same as the minimax with the difference
-                                                                                                            ###that now we have the pruning if w>a or w<b in the 2 "edges"
+                                                                                                            
+                                                                                                            
             for action in Actions:
                 sucsValue=min_value(gameState.generateSuccessor(0,action),1,depth,a,b)
                 sucsValue=sucsValue[0]
@@ -187,9 +187,9 @@ class AlphaBetaAgent(MultiAgentSearchAgent):
             Actions=gameState.getLegalActions(agentID) # Get the actions of the ghost
             if len(Actions) == 0:
                 return (self.evaluationFunction(gameState),None)
-                                                                                                            ###As we know from theory the alpha beta algorithms is an improved version
-                                                                                                            ###of the minimax in order to "pull through" some time,to have a better time
-                                                                                                            ###complexity
+                                                                                                           
+                                                                                                           
+                                                                                                          
             l = float("inf")
             Act = None
             for action in Actions:
@@ -206,7 +206,7 @@ class AlphaBetaAgent(MultiAgentSearchAgent):
 
                 b=min(b,l)
 
-            return(l,Act)                                                                                      ###I think there is nothing else to be said about this agent
+            return(l,Act)                                                                                      
 
         a=-(float("inf"))
         b=float("inf")
@@ -228,8 +228,8 @@ class ExpectimaxAgent(MultiAgentSearchAgent):
         "*** YOUR CODE HERE ***"
         def max_value(gameState,depth):
             Actions=gameState.getLegalActions(0)
-            if len(Actions)==0 or gameState.isWin() or gameState.isLose() or depth==self.depth:   ###The max "function" is exactly the same with the minimax and the difference is at
-                return (self.evaluationFunction(gameState),None)                                  ##exp,min "function" that now we have the probability
+            if len(Actions)==0 or gameState.isWin() or gameState.isLose() or depth==self.depth:  
+                return (self.evaluationFunction(gameState),None)                                 
 
             w=-(float("inf"))
             Act=None
@@ -238,9 +238,9 @@ class ExpectimaxAgent(MultiAgentSearchAgent):
                 sucsValue=exp_value(gameState.generateSuccessor(0,action),1,depth)
                 sucsValue=sucsValue[0]
                 if(w<sucsValue):
-                    w,Act=sucsValue,action                                                          ###Now the expectimax algorithm is the same as the minimax but now we have
-                                                                                                    ###also the probability we insert about each move that we maybe do as we 've been taught
-                                                                                                    ###in theory,similar things we have in chess and all the other games
+                    w,Act=sucsValue,action                                                          
+                                                                                                    
+                                                                                                   
             return(w,Act)
 
         def exp_value(gameState,agentID,depth):
@@ -270,8 +270,8 @@ def betterEvaluationFunction(currentGameState):
       DESCRIPTION: <write something here so we know what you did>
     """
     "*** YOUR CODE HERE ***"
-    pacPosition=currentGameState.getPacmanPosition()                                                     ###Now we do not want only the pacman,the food and the ghost positions
-    gList=currentGameState.getGhostStates()                                                              ###but also the capsules
+    pacPosition=currentGameState.getPacmanPosition()                                                     
+    gList=currentGameState.getGhostStates()                                                              
     Food=currentGameState.getFood()
     Capsules=currentGameState.getCapsules()
 
@@ -283,18 +283,18 @@ def betterEvaluationFunction(currentGameState):
     foodDistList=[]
     for food in Food.asList():
         foodDistList+=[util.manhattanDistance(food,pacPosition)]
-    minFDist=min(foodDistList)                                                                              ###We have a better evaluation function,what it means?
-    GhDistList=[]                                                                                           ###It means that we have take into account more parameters in order to have a better evalution function
-    ScGhDistList=[]                                                                                         ###Of course every parameter has its own "gravity,importance" like chess the strategical advantages
-    for ghost in gList:                                                                                     ###are less important than the tactical,material ones
+    minFDist=min(foodDistList)                                                                              
+    GhDistList=[]                                                                                           
+    ScGhDistList=[]                                                                                         
+    for ghost in gList:                                                                                     
         if ghost.scaredTimer==0:
             GhDistList+=[util.manhattanDistance(pacPosition,ghost.getPosition())]
         elif ghost.scaredTimer>0:
             ScGhDistList+=[util.manhattanDistance(pacPosition,ghost.getPosition())]
     minGhDist=-1
     if len(GhDistList) > 0:
-        minGhDist=min(GhDistList)                                                                             #We have the min distance of a ghost,the min distance of a scaredGhost,the amount of the capsules,the food and the min distance of a food.
-    minScGhDist=-1                                                                                            #As we see they do not hve all the same role-importance in the estimation -evaluation of a state
+        minGhDist=min(GhDistList)                                                                             
+    minScGhDist=-1                                                                                            
     if len(ScGhDistList)>0:
         minScGhDist=min(ScGhDistList)
     score=scoreEvaluationFunction(currentGameState)
